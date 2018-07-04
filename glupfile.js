0c0af24ac575c9e4c0bbb0f05207b5e24797053a
yalmayk <yalamyk@gmail.com>
2018-07-04 18:43:55 +0900

    * sprite 내용 추가

--- glupfile.js
+++ glupfile.js
@@ -1,12 +1,16 @@
 'use strict';
 
 const gulp = require('./node_modules/gulp');
-const sass = require('gulp-sass');
-const rename = require('gulp-rename');
-const sourceMaps = require('gulp-sourcemaps');
-const commentStrip = require('gulp-strip-comments');
-const replace = require('gulp-replace');
 const autoPrefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const sourceMaps = require('gulp-sourcemaps');
+const spriteSmith = require('gulp.spritesmith');
+// const px2vw = require('./script/px_2_vw');
+const gulpSync = require('gulp-sync')(gulp);
+const commentStrip = require('gulp-strip-comments');
+const cleanCSS = require('gulp-clean-css');
+const replace = require('gulp-replace');
 
 // sass
 gulp.task('sass', function () {
@@ -14,5 +18,23 @@ gulp.task('sass', function () {
         .pipe(sass({outputStyle : 'compact'}).on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'));
 });
+// image sprite
+gulp.task('sprite', function () {
+    let spriteData = gulp.src('./img/*.png', {read: false})
+        .pipe(spriteSmith({
+            retinaSrcFilter: ['./img/*_2x.png'],
+            imgName: '1x.png',
+            retinaImgName: '2x.png',
+            padding: 4,
+            cssName: 'sprite.scss',
+            // cssTemplate: './src/scss/template/scss.handlebars',
+            imgPath: '../img/1x.png',
+            retinaImgPath: '../img/2x.png'
+            //engine: phantomSmithJS
+        }));
+    spriteData.img.pipe(gulp.dest('./dist/img'));
+    spriteData.css.pipe(gulp.dest('./dist/scss'));
 
-gulp.task('all', ['sass']);
+    return spriteData;
+});
+gulp.task('all', ['sass','sprite']);
